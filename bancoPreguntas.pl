6625cb799b7f497d594bc596079cfc15b4826af8
AVBLANCO <BLANKO_1490@HOTMAIL.COM>
2020-06-01 17:23:35 -0500

    actualizacion banco pregutnas
    banco preguntas

--- bancoPreguntas.pl
+++ bancoPreguntas.pl
@@ -6,16 +6,19 @@ validarno(P,R) :- espregunta(P), esrespuestano(R).
 esrespuesta('si').
 esrespuestano('no').
 
+
 % Banco de preguntas sobre Codiv-19
-espregunta(0).
-espregunta('¿Usted tiene escalofrios?').
-espregunta('¿ha tenido Diarrea?').
-espregunta('¿tiene dolor de garganta?').
-espregunta('¿ha tenido dolores musculares?').
-espregunta('¿Tiene dolor de cabeza?').
-espregunta('¿ha tenido fiebre de 38° o mas?').
-espregunta('¿ha tenido dificultad para respirar?').
-espregunta('¿Ha sentido cansancio y/ó debilidad?').
-espregunta('¿ha viajado en los ultimos 14 dias?').
-espregunta('¿ha visitado areas infectadas por Covid 19?').
-espregunta('¿visito o ha cuidado pacientes con Covid 19 positivo?').
+espregunta(0). %¿Usted tiene tos?
+espregunta(1). %¿Usted tiene escalofrios?
+espregunta(2). %¿ha tenido Diarrea?
+espregunta(3). %¿tiene dolor de garganta?
+espregunta(4). %¿ha tenido dolores musculares?
+espregunta(5). %¿Tiene dolor de cabeza?
+espregunta(6). %¿ha tenido fiebre de 38° o mas?
+espregunta(7). %¿ha tenido dificultad para respirar?
+espregunta(8). %¿Ha sentido cansancio y/ó debilidad?
+espregunta(9). %¿ha viajado en los ultimos 14 dias?
+espregunta(10). %¿ha visitado areas infectadas por Covid 19?
+espregunta(11). %¿visito o ha cuidado pacientes con Covid 19 positivo?
+
+
